36cffbec0e799ccd1df3011b2db70aa785acf043
Senpai <ratulird@gmail.com>
2021-07-31 00:29:29 +0300

    Changing addElement function

--- todo_list.js
+++ todo_list.js
@@ -7,13 +7,23 @@ function addElement() {
 	let btn = document.createElement('button');
 	btn.setAttribute("onclick", "deleteElement()");
 	btn.className += "btn li__button";
-	let img = document.createElement('img');
-	img.setAttribute("src", "img/close.svg");
-	img.className += "close";
-	btn.appendChild(img);
+	let img_close = document.createElement('img');
+	img_close.setAttribute("src", "img/close.svg");
+	img_close.className += "close";
+	btn.appendChild(img_close);
+	let img_check = document.createElement('img');
+	img_check.setAttribute("src", "img/check.svg");
+	img_check.className += "check";
 	div.className += "li__text";
 	div.innerHTML = input_text;
+	div.appendChild(img_check);
 	li.appendChild(div);
 	li.appendChild(btn);
 	list.appendChild(li);
+	document.getElementById('input_add').value = "";
+	// appendChild(node) - старый код 
+}
+function deleteElement() {
+	// let elem = this;
+	// elem.remove();
 }
