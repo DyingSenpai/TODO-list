f46514057aba59a405b3e57363604e1811526588
Senpai <ratulird@gmail.com>
2021-07-31 13:26:38 +0300

    Add activate function

--- todo_list.js
+++ todo_list.js
@@ -1,11 +1,14 @@
 'use strict'
 function addElement() {
 	let input_text = document.getElementById('input_add').value; // Берём текст из инпута
+	if(input_text === "") {
+		alert("Поле не должно быть пустым!");
+		return;
+	}
 	let list = document.getElementById('myList'); // Берём айди списка
 	let li = document.createElement('li'); // Создаём пункт списка
 	let div = document.createElement('div'); // Создаём див для пункта списка
 	let btn = document.createElement('button');
-	//btn.setAttribute("onclick", "deleteElement()");
 	btn.className += "btn li__button close";
 	let img_close = document.createElement('img');
 	img_close.setAttribute("src", "img/close.svg");
@@ -13,11 +16,12 @@ function addElement() {
 	let img_check = document.createElement('img');
 	img_check.setAttribute("src", "img/check.svg");
 	img_check.className += "check";
-	div.className += "li__text";
+	div.className += "li__text ";
 	div.innerHTML = input_text;
 	div.appendChild(img_check);
 	li.appendChild(div);
 	li.appendChild(btn);
+	li.setAttribute("id", "myList_li");
 	list.appendChild(li);
 	document.getElementById('input_add').value = "";
 	// appendChild(node) - старый код 
@@ -30,3 +34,9 @@ document.querySelector('ul').onclick = function(e) {
 	}
 	btn.parentElement.remove();
 }
+let list = document.querySelector('ul');
+list.addEventListener('click', function(ev) {
+	if (ev.target.tagName === 'LI') {
+    	ev.target.classList.toggle('active');
+	}
+}, false);
